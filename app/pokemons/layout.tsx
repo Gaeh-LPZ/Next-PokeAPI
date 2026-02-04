69ab40b1bb61839eea1efffc796c07ec8130637f
Gaeh-LPZ <galob567@gmail.com>
2026-02-03 21:17:34 -0600

    Desktop version finished

--- app/pokemons/layout.tsx
+++ app/pokemons/layout.tsx
@@ -2,9 +2,9 @@ import { Navbar } from "../ui/navbar";
 
 export default function Layout({children}: {children: React.ReactNode;}) {
   return (
-    <div className="flex w-full">
+    <div className="flex min-h-screen ">
       <Navbar/>
-      <div className="grid grid-cols-3 gap-3 m-4 w-full items-center justify-between">
+      <div className="grid grid-cols-3 gap-3 m-4 w-full items-center justify-items-center">
         {children}
       </div>
     </div>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,7 +1,22 @@
-export function Navbar(){
+import { fetchPokemonDetail } from "../lib/data";
+import Image from "next/image";
+
+export async function Navbar(){
+    const ids = [1,2,3,4,5,6,7,8,9]
+
+    const pokemonList = await Promise.all(ids.map(id => fetchPokemonDetail(id)));
+
     return(
-        <aside className="sm:hidden md:flex flex-col w-40 m-4 items-center p-3 gap-5">
-            <a className="hover:bg-yellow-300/95 hover:text-black w-full text-center rounded-md hover:scale-110 hover:transform transition" href="#">1</a>
+        <aside className="sm:hidden md:flex flex-col w-40 m-4 items-center p-3 gap-5 bg-background/90">
+            <Image
+                src="/pokemon-logo.webp"
+                alt="Logo of pokemon"
+                width={80}
+                height={50}
+            />
+            {pokemonList.map((poke) => (
+                <a key={poke.id} className="hover:bg-yellow-300/95 hover:text-black w-full text-center rounded-md hover:scale-110 hover:transform transition capitalize" href="#">{poke.name}</a>
+            ))}
         </aside>
     );
 }
